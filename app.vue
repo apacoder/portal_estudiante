700b96b128c1f919c3a0838aa49e2bf2796bae10
Rafael Aparicio <raparicio@gmail.com>
2024-07-18 18:13:10 -0600

    Persistiendo sesion

--- app.vue
+++ app.vue
@@ -1,6 +1,33 @@
-import { NuxtLayout } from './.nuxt/components';
+<script setup>
+import { api_gestion_autorizacion } from './server/axios'
+
+const route = useRoute()
+
+const user = useState('user')
+
+const is_loaded = ref(false)
+
+const fetchUser = async () => {
+  try {
+    const response = await api_gestion_autorizacion.get('/auth/user')
+    console.log(response)
+    user.value = response.data
+    is_loaded.value = true
+  } catch (error) {
+    is_loaded.value = true
+    console.log('errorsisto')
+    if( route.fullPath !== '/login') {
+      window.location.href = '/login'
+    }
+  }
+}
+fetchUser()
+</script>
+
 <template>
-<NuxtLayout>
+<b>{{ route.fullPath }}</b>
+<pre>{{ user }}</pre>
+<NuxtLayout v-if="route.fullPath == '/login' || is_loaded">
   <NuxtPage />
 </NuxtLayout>
 </template>
--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,38 +1,5 @@
-import { api_gestion_autorizacion, setCSRFToken } from '~/server/axios'
-
 export default defineNuxtRouteMiddleware(async to => {
-  const user = useState('user')
-  
-  const is_index = to.path === '/'
-  const is_login = to.path === '/login'
-  const is_auth = toRaw(user.value)
-
-  try {
-    if(!is_auth) {
-      const {data} = await api_gestion_autorizacion.post('/google_one_tap/refresh_token')
-      user.value = {
-        nombre: 'nombre',
-        apellido: 'apellido',
-      }
-      console.log(data)
-      
-      setCSRFToken(data.ga_csrf_token)
-    }
-  } catch (error) {
-    if (to.path != '/login') 
-      return navigateTo('/login')
+  if (to.fullPath == '/') {
+    return navigateTo('/inicio')
   }
-
-
-  // Si el usuario está autenticado y va a /login o /, redirigir a /inicio
-  if (is_auth && (is_login || is_index)) 
-    return navigateTo('/inicio')
-  
-  // Si el usuario no está autenticado y no va a /login, redirigir a /login
-  if (!is_auth && !is_login) 
-    return navigateTo('/login')
-
-  // Si el usuario está autenticado y no va a /login, continuar normalmente
-  if (is_index) 
-    return navigateTo('/inicio')
 })
